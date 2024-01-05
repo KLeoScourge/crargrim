5955370bc7bfa0183cfc18405bf1e5840345608e
KLeoScourge <kleonardiaz2886@gmail.com>
2024-01-05 13:50:32 -0500

    Actualizacion

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ class VehProduct {
         this.vehimg = vehimg;
     }
 };
+class AccProduct {
+    constructor(accname, accprice, accdesc, accimg){
+        this.accname = accname;
+        this.accprice = accprice;
+        this.accdesc = accdesc;
+        this.accimg = accimg;
+    }
+};
 const perproductos = [new PerProduct('Manilla en cuero con dijes dama', 25000, 'Manilla en cuero', 'Manilla dijes mujer.jpg'), 
                       new PerProduct('Manilla taches cadena en cuero dama', 20000, 'Manilla en cuero', 'Manilla taches cadena mujer.jpg'),
                       new PerProduct('Pechero gótico en cuero mujer', 35000, 'Pechero en cuero', 'Pechero sencillo  mujer.jpg'),
@@ -24,8 +32,9 @@ const perproductos = [new PerProduct('Manilla en cuero con dijes dama', 25000, '
 
 const vehproductos = [new VehProduct('Flecos en cuero no cubre manubrio para moto', 130000, 'Flecos en cuero', 'Flecos_manilar_corto.jpg'), 
                       new VehProduct('Banderilla delantera para moto', 30000, 'Banderilla en cuero', 'Banderilla_delantera_moto.jpg'),
-                      new VehProduct('Cubre tanque en cuero para moto GN', 90000, 'Cubre tanque en cuero', 'Cubre_tanque_GN.jpg'),
-                      new VehProduct('Llavero murciélago en cuero cubre llaves', 20000, 'Llavero en cuero', 'Llavero_murciélago.jpg')]                 
+                      new VehProduct('Cubre tanque en cuero para moto GN', 90000, 'Cubre tanque en cuero', 'Cubre_tanque_GN.jpg')]               
+
+const accproductos = [new AccProduct('Llavero murciélago en cuero cubre llaves', 20000, 'Llavero en cuero', 'Llavero_murciélago.jpg')]                 
 function mostrarPerproductos(){
     console.log('Mostrando Perproductos')
     let Pertexto = '';
@@ -50,11 +59,30 @@ function mostrarVehproductos(){
     }
     document.getElementById('productosveh').innerHTML = Vehtexto;
 }
+function mostrarAccproductos(){
+    console.log('Mostrando Accproductos')
+    let Acctexto = '';
+    for (const accproducto of accproductos) {
+        Acctexto += `<div class="element">
+                     <img src="assets/${accproducto.accimg}" style="width: 340px; height: 340px;">
+                     <h3>${accproducto.accname}</h3>
+                     <h4>${accproducto.accprice}</h4>
+                  </div>`;
+    }
+    document.getElementById('productosacc').innerHTML = Acctexto;
+}
 function personales(){
     document.getElementById('ProdPersonales').style.display = '';
     document.getElementById('ProdVehiculares').style.display = 'none';
+    document.getElementById('ProdAccesorios').style.display = 'none';
 }
 function motocicleta(){
     document.getElementById('ProdPersonales').style.display = 'none';
     document.getElementById('ProdVehiculares').style.display = '';
+    document.getElementById('ProdAccesorios').style.display = 'none';
+}
+function accesorios(){
+    document.getElementById('ProdPersonales').style.display = 'none';
+    document.getElementById('ProdVehiculares').style.display = 'none';
+    document.getElementById('ProdAccesorios').style.display = '';
 }
